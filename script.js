30a39162de363658479930170a65312f57c34858
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2024-09-18 15:01:13 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// Function to add
+function addNumbers(){
+    num1 + num2;
+}
+
+// Function to subtract
+function subNumbers(){
+    num1 - num2;
+}
+
+// Function to multiply
+function multiplyNumbers(){
+    num1 * num2;
+}
+
+// Function to divide
+function divNumbers(){
+    num1 / num2;
+}
